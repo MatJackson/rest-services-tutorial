053577977eb9a6b47ace523fb4495f476570ac46
MJ <mjackson74u@gmail.com>
2019-05-18 19:26:13 -0400

    init commit

---
+++ src/main/java/com/jackson/restservicestutorial/RestServicesTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.jackson.restservicestutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestServicesTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestServicesTutorialApplication.class, args);
+	}
+
+}
