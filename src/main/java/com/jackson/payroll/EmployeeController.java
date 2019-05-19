f6e36ffd34e623116aa0bdfa138adf182d0dfefe
MJ <mjackson74u@gmail.com>
2019-05-18 21:34:11 -0400

    add links to rest service

--- src/main/java/com/jackson/payroll/EmployeeController.java
+++ src/main/java/com/jackson/payroll/EmployeeController.java
@@ -1,8 +1,15 @@
 package com.jackson.payroll;
 
+import org.springframework.hateoas.Resource;
+import org.springframework.hateoas.Resources;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
+
 
 @RestController
 public class EmployeeController {
@@ -14,8 +21,16 @@ public class EmployeeController {
     }
 
     @GetMapping("/employees")
-    List<Employee> all() {
-        return repository.findAll();
+    Resources<Resource<Employee>> all() {
+
+        List<Resource<Employee>> employees = repository.findAll().stream()
+                .map(employee -> new Resource<>(employee,
+                        linkTo(methodOn(EmployeeController.class).one(employee.getId())).withSelfRel(),
+                        linkTo(methodOn(EmployeeController.class).all()).withRel("employees")))
+                .collect(Collectors.toList());
+
+        return new Resources<>(employees,
+                linkTo(methodOn(EmployeeController.class).all()).withSelfRel());
     }
 
     @PostMapping("employees")
@@ -24,9 +39,14 @@ public class EmployeeController {
     }
 
     @GetMapping("/employees/{id}")
-    Employee one(@PathVariable Long id) {
-        return repository.findById(id)
+    Resource<Employee> one(@PathVariable Long id) {
+
+        Employee employee = repository.findById(id)
             .orElseThrow(() -> new EmployeeNotFoundException(id));
+
+        return new Resource<>(employee,
+                linkTo(methodOn(EmployeeController.class).one(id)).withSelfRel(),
+                linkTo(methodOn(EmployeeController.class).all()).withRel("employees"));
     }
 
     @PutMapping("/employees/{id}")
